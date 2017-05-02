aa07fc051097d3a8198771798a6a1e7e0b3d38db
sinchang <sinchangwen@gmail.com>
2017-05-02 22:06:53 +0800

    init

---
+++ data.js
@@ -0,0 +1,132 @@
+export default [{
+    'name': 'element',
+    'url': 'https://github.com/ElemeFE/element',
+    'author': 'ElemeFE',
+    'tags': [
+      'vue',
+      'components',
+      'ui-kit'
+    ],
+    'description': 'A Vue.js 2.0 UI Toolkit for Web'
+  },
+  {
+    'name': 'buefy',
+    'url': 'https://github.com/rafaelpimpa/buefy',
+    'author': 'rafaelpimpa',
+    'tags': [
+      'vue',
+      'bulma',
+      'components',
+      'ui-kit'
+    ],
+    'description': 'Lightweight UI components for Vue.js based on Bulma'
+  },
+  {
+    'name': 'vue-toasted',
+    'url': 'https://github.com/shakee93/vue-toasted',
+    'author': 'shakee93',
+    'tags': [
+      'vue',
+      'toast',
+      'components'
+    ],
+    'description': 'Responsive Touch Compatible Toast plugin for VueJS 2+'
+  },
+  {
+    'name': 'vue-timeago',
+    'url': 'https://github.com/egoist/vue-timeago',
+    'author': 'egoist',
+    'tags': [
+      'vue',
+      'timeago',
+      'components'
+    ],
+    'description': 'A tiny timeago component for Vue'
+  },
+  {
+    'name': 'vue-social-sharing',
+    'url': 'https://github.com/nicolasbeauvais/vue-social-sharing',
+    'author': 'nicolasbeauvais',
+    'tags': [
+      'vue',
+      'social-shares',
+      'components'
+    ],
+    'description': 'A Vue.js component for sharing links to social networks'
+  },
+  {
+    'name': 'vue-baidu-map',
+    'url': 'https://github.com/Dafrok/vue-baidu-map',
+    'author': 'Dafrok',
+    'tags': [
+      'vue',
+      'map',
+      'baidu',
+      'baidumap'
+    ],
+    'description': 'Baidu Map components for Vue 2.x'
+  },
+  {
+    'name': 'vonic',
+    'url': 'https://github.com/wangdahoo/vonic',
+    'author': 'wangdahoo',
+    'tags': [
+      'vue',
+      'mobile',
+      'ionic',
+      'ui',
+      'components'
+    ],
+    'description': 'Baidu Map components for Vue 2.x'
+  },
+  {
+    'name': 'vue-amap',
+    'url': 'https://github.com/ElemeFE/vue-amap',
+    'author': 'ElemeFE',
+    'tags': [
+      'vue',
+      'map',
+      'components'
+    ],
+    'description': '基于 Vue 2.x 和高德地图的地图组件'
+  },
+  {
+    'name': 'iview',
+    'url': 'https://github.com/iview/iview',
+    'author': 'iview',
+    'tags': [
+      'vue',
+      'ui-kit',
+      'ui-design',
+      'components'
+    ],
+    'description': 'A high quality UI Toolkit built on Vue.js'
+  },
+  {
+    'name': 'radon-ui',
+    'url': 'https://github.com/luojilab/radon-ui',
+    'author': 'luojilab',
+    'tags': [
+      'vue',
+      'ui-kit',
+      'ui-design',
+      'components'
+    ],
+    'description': '一个用于快速开发产品的简洁、优雅、高效的 Vue 组件库'
+  },
+  {
+    'name': 'vux',
+    'url': 'https://github.com/airyland/vux',
+    'author': 'airyland',
+    'tags': [
+      'vue',
+      'ui-kit',
+      'ui-design',
+      'weui',
+      'wechat',
+      'mobile',
+      'components'
+    ],
+    'description': 'Vue UI Components based on WeUI'
+  }
+]
---
+++ src/components/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+    <github-badge slug="sinchang/vuemarket" />
+    <h1 class="title">Vue Market</h1>
+    <p class="subtitle">Fantastic Vue Components and Libraries.</p>
+    <nav class="panel">
+      <p class="panel-heading">
+        Repositories
+      </p>
+      <div class="panel-block">
+        <p class="control has-icons-left">
+          <input class="input" type="text" placeholder="Search" v-model="keyword" @input="handleChange">
+          <span class="icon is-small is-left">
+            <i class="fa fa-search"></i>
+          </span>
+        </p>
+      </div>
+      <a class="panel-block" v-for="repo in repos" :href="repo.url" target="_blank">
+        <span class="panel-icon">
+          <i class="fa fa-book"></i>
+        </span> {{ repo.author }}/{{ repo.name }}
+        <br> {{ repo.description }}
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+  import GitHubBadge from 'vue-github-badge'
+  import debounce from 'lodash.debounce'
+  import repos from '../../data'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        repos,
+        keyword: ''
+      }
+    },
+    methods: {
+      handleChange: debounce(function () {
+        this.repos = repos
+        if (!this.keyword) {
+          return
+        }
+        this.repos = this.repos.filter(repo => {
+          return repo.name.toLowerCase().indexOf(this.keyword) > -1 ||
+            repo.description.toLowerCase().indexOf(this.keyword) > -1 ||
+            repo.tags.indexOf(this.keyword) > -1 ||
+            repo.author.indexOf(this.keyword) > -1
+        })
+      }, 300)
+    },
+    components: {
+      'github-badge': GitHubBadge
+    }
+  }
+</script>
+
+<style>
+  html,
+  body,
+  #app {
+    height: 100%;
+  }
+
+  #app {
+    margin: 30px auto;
+    max-width: 800px;
+  }
+
+  .title {
+    color: #00d1b2;
+    font-weight: 700;
+    font-size: 46px;
+  }
+
+  .repo-meta {
+    color: #00d1b2;
+  }
+
+  .panel {
+    margin-bottom: 1.5rem;
+  }
+
+  body {
+    background-color: #fff;
+    margin: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 768px) {
+    #app {
+      width: 90%;
+    }
+  }
+</style>
